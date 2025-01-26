59d80c987ecd63ae521a2562c54f4e925c9706f0
miyahara <miyahara@engimiya.com>
2025-01-26 23:36:28 +0900

    5days task done. accountCreator.

---
+++ force-app/main/default/lwc/accountCreator/accountCreator.js
@@ -0,0 +1,46 @@
+//  基底クラスとapiデコレータをインポート
+import { LightningElement, api } from 'lwc';
+// createaAccountRecordメソッドをインポート
+import createAccountRecord from '@salesforce/apex/AccountController.createAccountRecord';
+// トースト通知を表示するためのモジュールをインポート
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+export default class AccountCreator extends LightningElement {
+    // 入力された取引先の値を格納するプロパティを定義 (慣習的に変数宣言ワードは付けない)
+    accountName = '';
+    
+    // 取引先名を変更したときに実行されるハンドラ関数
+    handleNameChange(event) {
+        // 入力フィールドの値を accountName プロパティに代入
+        this.accountName = event.target.value;
+    }
+    // 取引先を作成ボタンを押したときに実行されるハンドラ関数
+    handleCreateAccount() {
+        // ApexのcreateAccountRecordメソッドを実行
+        // ApexメソッドcreateAccountRecordの引数名がnameであるため、
+        // オブジェクトリテラルでnameプロパティに値を設定。
+        createAccountRecord({ name: this.accountName})
+            .then((result) => {
+                console.log('作成した取引先ID: ' + result);
+                this.accountName = '';
+                // 組み込みのdispatchEventを使用してトースト表示をする
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: '成功',
+                        message: '取引先が作成されました！ID: ' + result,
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'エラー',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+    }   
+}
