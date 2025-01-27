f76dc5c6c0c3107be8a406ca39f09fe6aa33e3d1
miyahara <miyahara@engimiya.com>
2025-01-27 18:55:07 +0900

    Added accountGetter, Datatable.

--- force-app/main/default/lwc/accountGetter/accountGetter.js
+++ force-app/main/default/lwc/accountGetter/accountGetter.js
@@ -1,3 +1,53 @@
-import { LightningElement } from 'lwc';
+// 基底クラスをインポート
+import { LightningElement, api } from 'lwc';
+// ApexのgetAccountRecordsをインポート
+import getAccountRecords from '@salesforce/apex/AccountController.getAccountRecords';
+// トースト表示するためのモジュールをインポート
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class AccountGetter extends LightningElement {}
+// Id, Name, Phone, Website
+const columns = [
+    { label: 'Name', fieldName: 'Name'},
+    { label: 'Phone', fieldName: 'Phone'},
+    { label: 'Website', fieldName: 'website'}
+];
+
+export default class AccountGetter extends LightningElement {
+    accoutName = '';
+    columns = columns;
+    data = [];
+
+    // 取引先の入力が変わったときに発生するイベントハンドラー関数
+    handleChangeAccountName(event) {
+        this.accoutName = event.target.value;
+    }
+
+    //取引先を検索ボタンを押したときに発生するイベントハンドラー関数
+    handleGetAccountRecords() {
+        getAccountRecords({name: this.accoutName})
+            .then((result) => {
+                // 取得した取引先の数
+                console.log('表示した取引先の数: ' + result.length);
+                // 取得したレコードを格納
+                this.data = result;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: '成功',
+                        message: result.length + ' 件 取引先レコードを表示しました。' + this.data[0].Name,
+                        variant: 'success'
+                    })
+                );
+                this.accoutName = '';
+                this.template.querySelector('lightning-input').value = this.accoutName;
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'エラー',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+}
